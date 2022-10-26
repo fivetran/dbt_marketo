40151396c9f915fa26b009a0bf5bb6822ea11ded
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2022-10-26 11:36:09 -0700

    buildkite

--- models/intermediate/marketo__change_data_scd.sql
+++ models/intermediate/marketo__change_data_scd.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized='incremental',
-        partition_by = {'field': 'valid_to', 'data_type': 'date'} if target.type not in ['!!!!!!! REPLACE 'spark' WITH 'spark','databricks' OR EQUIV !!!!!!!', 'databricks'] else ['valid_to'],
+        partition_by = {'field': 'valid_to', 'data_type': 'date'} if target.type not in ['spark','databricks'] else ['valid_to'],
         unique_key='lead_day_id',
         incremental_strategy='merge' if target.type not in ['postgres', 'redshift'] else 'delete+insert',
         file_format='delta'
