b08a547df9cb09f5102538d5d431aff1ffa4235b
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2024-11-01 15:21:12 -0500

    update test and yml

--- integration_tests/tests/consistency/consistency_leads.sql
+++ integration_tests/tests/consistency/consistency_leads.sql
@@ -4,42 +4,21 @@
 ) }}
 
 with prod as (
-    select {{ dbt_utils.star(from=ref('marketo__leads'), except=var('consistency_test_exclude_metrics', [])) }}
+    select count(*) as prod_row_count
     from {{ target.schema }}_marketo_prod.marketo__leads
 ),
 
 dev as (
-    select {{ dbt_utils.star(from=ref('marketo__leads'), except=var('consistency_test_exclude_metrics', [])) }}
+    select count(*) as dev_row_count
     from {{ target.schema }}_marketo_dev.marketo__leads
 ), 
 
-prod_not_in_dev as (
-    -- rows from prod not found in dev
-    select * from prod
-    except distinct
-    select * from dev
-),
-
-dev_not_in_prod as (
-    -- rows from dev not found in prod
-    select * from dev
-    except distinct
-    select * from prod
-),
-
-final as (
-    select
-        *,
-        'from prod' as source
-    from prod_not_in_dev
-
-    union all -- union since we only care if rows are produced
-
-    select
-        *,
-        'from dev' as source
-    from dev_not_in_prod
+count_check as (
+    select *
+    from prod
+    join dev
+        on prod.prod_row_count != dev.dev_row_count
 )
 
 select *
-from final
+from count_check
