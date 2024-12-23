8e0142fe3c17acc858760fb584c9fffefd5cb400
Renee Li <renee.li@fivetran.com>
2024-12-24 01:20:10 +0800

    add consistency tests and some commented out variables to have in place for testing

---
+++ integration_tests/tests/consistency/consistency_campaigns.sql
@@ -0,0 +1,24 @@
+{{ config(
+    tags="fivetran_validations",
+    enabled=var('fivetran_validation_tests_enabled', false)
+) }}
+
+with prod as (
+    select count(*) as prod_row_count
+    from {{ target.schema }}_marketo_prod.marketo__campaigns
+),
+
+dev as (
+    select count(*) as dev_row_count
+    from {{ target.schema }}_marketo_dev.marketo__campaigns
+), 
+
+count_check as (
+    select *
+    from prod
+    join dev
+        on prod.prod_row_count != dev.dev_row_count
+)
+
+select *
+from count_check
