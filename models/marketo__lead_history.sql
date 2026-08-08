415f67ca47268c368a18f129479d67d9a0218398
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-08-08 08:22:54 -0500

    fix for duckdb 1.11

--- models/marketo__lead_history.sql
+++ models/marketo__lead_history.sql
@@ -1,7 +1,7 @@
 {{
     config(
         materialized='incremental',
-        partition_by = {'field': 'date_day', 'data_type': 'date'} if target.type not in ['spark','databricks'] else ['date_day'],
+        partition_by = {'field': 'date_day', 'data_type': 'date'} if target.type not in ['spark','databricks','duckdb'] else ['date_day'],
         unique_key='lead_history_id',
         incremental_strategy='merge' if target.type not in ['postgres', 'redshift'] else 'delete+insert',
         file_format='delta'
